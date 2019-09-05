ebfb738f156de84cfc598134746a71165e0ce85b
yangyang <www.yangyang.com@qq.com>
2019-09-05 17:39:09 +0800

    nginx

--- updateREADME.sh
+++ updateREADME.sh
@@ -1,19 +1,17 @@
 #!/bin/bash 
 #更新README的脚本说明
 
-filedir=$1
-readmedir=$2
-
-Script=$(find $1 -iname "0*.sh" |sort -n )
-#echo $Script
+cd $1
+ 
+Script=`find  . -iname "[0-9]*.sh" |sort -n `
 for files in $Script
 do
  #echo $files
  Number=$(echo "$files" | cut -c 3-5 )
  Mask=$(grep "explain" $files)
- if [[ -z $(cat ./README.md | grep  "$Mask" ) ]];
+ if [[ -z $(cat README.md | grep  "$Mask" ) ]]
  then 
-   echo -e "\n"$Number"\n"$Mask >> $2'/README.md'
+   echo -e "\n"$Number"\n"$Mask >> README.md
  fi 
 done
 
